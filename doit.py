2bce18247199938ba46fefbd8d7d9d591f051a34
zvovov <zvovov@gmail.com>
2016-07-11 23:10:14 +0530

    Wireframe ready

---
+++ doit.py
@@ -0,0 +1,14 @@
+# Input tv show name
+
+# Look it up on Tunefind
+    # Not Found
+        # Exit
+    # Found
+        # Read HTML containing Season list
+        # Go to each season
+            # Read HTML containing Songs list
+            # Extract song names
+            # Store song names
+        # Display song names
+
+# Download songs - WIP
