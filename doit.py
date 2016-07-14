c54929697277c1d481ca3a6d81b3485681195ef3
zvovov <zvovov@gmail.com>
2016-07-14 15:27:28 +0530

    saving progress

--- doit.py
+++ doit.py
@@ -21,7 +21,6 @@ def getSlug(_name):
 def getSoup(_url, _type, _name, _season, _episode_link):
     """
     Returns a html-parsed-BeautifulSoup object for the given URL.
-    
     @params:
     _url: String: the http(s) link to get html from
     _type: String: show|movie
@@ -62,12 +61,12 @@ def getSoup(_url, _type, _name, _season, _episode_link):
 #                 nums = range(int(nums[0]), int(nums[1]))
 #             else:
 #                 raise ValueError
-        
+
 #         elif _entered.count('-') > 1:
 #             raise ValueError
 
 #     except ValueError:
-#         print("Why you gotta be that way? Type correctly now: ")   
+#         print("Why you gotta be that way? Type correctly now: ")
 #         return False
 
 #     return nums
@@ -76,22 +75,22 @@ def getSoup(_url, _type, _name, _season, _episode_link):
 if __name__ == "__main__":
     cont_season = 'y'
     _season = 1
-    _name = getSlug("game of thrones")
-    
+    _name = getSlug("orange is the new black")
+    print(_name)
     soup = getSoup(_base_url, _type, _name, _season, "")
 
     _season_count = soup.find_all('ul')[1]
-    _season_count = (_season_count.find_all('li')[-1].find('a')['href'])[-1]
+    _season_count = int((_season_count.find_all('li')[-1].find('a')['href'])[-1])
     print("Seasons Found:", _season_count)
     while cont_season == 'y' or cont_season == 'Y':
         _season_wanted = input("Which season's songs do you want to see? 1 to {} (0 for all): ".format(_season_count))
         _season_wanted = int(_season_wanted)
-        _season_start = 0 if _season_wanted == 0 else _season_wanted
-        _season_end = _season_count if _season_wanted == 0 else _season_wanted + 1
+        _season_start = 1 if _season_wanted == 0 else _season_wanted
+        _season_end = _season_count + 1 if _season_wanted == 0 else _season_wanted + 1
 
         for curr_season in range(_season_start, _season_end):
-            curr_soup = getSoup(_base_url, _type, _name, curr_season, "")
-            _lists = curr_soup.find_all('li')
+            _season_soup = getSoup(_base_url, _type, _name, curr_season, "")
+            _lists = _season_soup.find_all('li')
             _episode_count = 0
             _link_dict = {}
             for i in _lists:
@@ -117,29 +116,35 @@ if __name__ == "__main__":
 
             _episode_wanted = input("Which episode's songs do you want to see? 1 to {} (0 for all): ".format(_episode_count))
             _episode_wanted = int(_episode_wanted)
-            _episode_start = 0 if _episode_wanted == 0 else _episode_wanted
-            _episode_end = _episode_count if _episode_wanted == 0 else _episode_wanted + 1
+            _episode_start = 1 if _episode_wanted == 0 else _episode_wanted
+            _episode_end = _episode_count + 1 if _episode_wanted == 0 else _episode_wanted + 1
 
             for curr_episode in range(_episode_start, _episode_end):
-                episode_soup = getSoup(_base_url, _type, _name, curr_season, _link_dict[curr_episode])
-                print(episode_soup.prettify())
-        cont_season = input("More Seasons? y/n ")
+                if _link_dict.get(curr_episode):
+                    # if curr_episode contains non-zero songs, get songs.
+                    _episode_soup = getSoup(_base_url, _type, _name, curr_season, _link_dict[curr_episode])
+                    _e_title = _episode_soup.title.string[:-11]
+                    print(_e_title)
+                    print(_episode_soup.get_text())
 
+                    # print(episode_soup.prettify())
+        
+        cont_season = input("More Seasons? y/n ")
 
 
 # Input tv show name
 
 # Look it up on Tunefind
-    # Not Found
-        # Exit
-    # Found
-        # Read HTML containing Season list
-        # Go to each season
-                # Go to each episode
-                    # Read HTML containing Songs list
-                    # Extract song names
-                    # Store song names
-            # Display song names
-        # Display summary
+#     Not Found
+#         Exit
+#     Found
+#         Read HTML containing Season list
+#         Go to each season
+#                 Go to each episode
+#                     Read HTML containing Songs list
+#                     Extract song names
+#                     Store song names
+#             Display song names
+#         Display summary
 
 # Download songs - WIP
