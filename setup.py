0f3f031399af553c47091b1541790f73c601b5e8
zvovov <zvovov@gmail.com>
2016-07-28 02:16:13 +0530

    0.2 version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tvsongs',
-      version='0.2.dev',
+      version='0.2',
       description='Find and listen to songs featured in TV shows.',
       keywords='find search listen tv show series songs music featured',
       url='https://github.com/zvovov/tvsongs',
